1bfb59f3e3fa26471a9853bf915ffb9c6ed3536f
monishwarmc <monishwar69@gmail.com>
2025-05-01 21:00:35 +0530

    small bug

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ fastify.get('/terms/:id', async (request, reply) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen({ port: PORT, host: '0.0.0.0' }, err => {
+fastify.listen({ port: PORT, host: '0.0.0.0' }, err => {
   if (err) {
     console.error(err);
     process.exit(1);
