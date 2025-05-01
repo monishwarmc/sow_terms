6786e9b3c3468aa87268d51b07edd8c8df606c67
monishwarmc <monishwar69@gmail.com>
2025-05-01 20:59:38 +0530

    small bug

--- react/src/utils/api.js
+++ react/src/utils/api.js
@@ -1,7 +1,7 @@
 // utils/api.js
 import axios from 'axios';
 
-const BASE_URL = 'https://sow-terms-backend.onrender.com/';
+const BASE_URL = 'https://sow-terms-backend.onrender.com';
 
 export const getTermById = async (id) => {
   const response = await axios.get(`${BASE_URL}/terms/${id}`);
